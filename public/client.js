1a6a367c58a83e9705876ab4c9b65a72814d17cd
Elliot Thomas <elliotnthomas@gmail.com>
2016-12-11 20:45:49 -0600

    added styling to the buttons

--- public/client.js
+++ public/client.js
@@ -1,46 +1,57 @@
-console.log("In JS");
+// console.log("In JS");
+
+//GLOBAL VARIABLES//
 var numArray = [];
 var currentOperator;
+
 //BEGIN DOCUMENT READY//
 $(document).ready (function(){
-console.log("in JQ");
+// console.log("in JQ");
 
 enableButtons ();
 
 });
 //END DOC READY//
 
+//FUNCTIONS//
+
 function enableButtons (){
   $(document).on('click', '.number-button', numButtonClick);
   $(document).on('click', '.operator-button', operatorButtonClick);
   $(document).on('click', '.equal-button', postDataOnEqual);
+  $(document).on('click', '#clearButton', clearAll);
 
-};//clicked buttons
+};//end enable buttons
 
 function numButtonClick (){
-  var clickedButton = $(this).data().value
-  console.log(clickedButton);
-  var prevValue = '';
-  prevValue = $('.output-box').val();
-  $('.output-box').val(prevValue + clickedButton);
+    var clickedButton = $(this).data().value;
+    var prevValue = '';
+    prevValue = $('.output-box').val();
+    $('.output-box').val(prevValue + clickedButton);
 };//end num button click
 
 function operatorButtonClick (){
   var currentValue = $('.output-box').val();
   numArray.push(currentValue)
-  console.log("current value is:",currentValue);
+  // console.log("current value is:",currentValue);
   var clickedButton = $(this).data().value
   $('.output-box').val(clickedButton);
   currentOperator = $('.output-box').val();
   $('.output-box').val("");
-  console.log(currentOperator);
+  // console.log(currentOperator);
 };//end operator Button Click
-
 
 function postDataOnEqual () {
 
+  if (numArray[numArray.length - 2] == null && numArray[numArray.length - 1] == null){
+    return
+  } else {
+    var currentValue = $('.output-box').val();
+    numArray.push(currentValue)
+
   var objectToSend = {
-    numbers: numArray,
+    number1: numArray[numArray.length - 2],
+    number2: numArray[numArray.length - 1],
     operator: currentOperator
   }; //end object to send
 
@@ -49,12 +60,40 @@ function postDataOnEqual () {
     url: '/sendObject',
     data: objectToSend,
     success: function(response){
-      console.log('back from post call:', response);
+      // console.log('back from post call:', response);
     },
     error: function (){
-      console.log('error with ajax call...');
+      // console.log('error with ajax call...');
     }
   });//end ajax call
 
+  numArray = [];
+  currentOperator = '';
+  $('.output-box').val('');
+
+  getData();
+}//end else stmt
 
 };//end post data
+
+function getData (){
+  // console.log("in get");
+  $.ajax({
+    type: "GET",
+    url: "/sendAnswer",
+    success: function(response){
+      // console.log('back from post call:', response);
+      // console.log(response.answer);
+      $('.output-box').val(response.answer);
+      },
+      error: function (){
+        console.log('error with ajax call...');
+      }
+    });//end ajax call
+};//get data
+
+function clearAll () {
+  $('.output-box').val('');
+  numArray = [];
+  currentOperator = '';
+};//clear all
--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var urlEncodedParser = bodyParser.urlencoded ({extended: false});
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
+
+var answerArray = [];
 
 app.listen (port, function (req,res){
   console.log( 'server is listening on', port);
@@ -15,6 +17,36 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve('public/index.html'));
 });//end base url
 
+//send info back
+app.get('/sendAnswer', function(req, res){
+  console.log('base url hit');
+  var objectToSend = {
+    answer: answerArray[answerArray.length - 1]
+  };
+  res.send(objectToSend);
+});//send info back
+
 app.post('/sendObject', urlEncodedParser, function ( req, res){
   console.log('send guess url hit', req.body);
+  var num1 = req.body.number1;
+  var num2 = req.body.number2;
+  var operator = req.body.operator;
+  console.log(operator);
+  console.log(num1);
+  console.log(num2);
+
+  if (operator == "+"){
+    answer = parseInt(num1) + parseInt(num2)
+  } else if (operator == "-"){
+    answer = num1 - num2
+  } else if (operator == "*"){
+    answer = num1 * num2
+  } else {
+    answer = num1/num2
+  } //end if else
+
+  answerArray.push(answer)
+
 });//end post
+
+app.use(express.static(path.join(__dirname, '../public/')));
